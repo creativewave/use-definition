e601ac5d5e24395525ab049474285bf03dce1f6d
Guillaume <gui.lebas@gmail.com>
2019-04-30 12:18:23 +0200

    fix(.babelrc) Fix dist/es build

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,11 @@
 
-// BABEL_ENV: module definition system (esm, cjs, or false ie. handled by webpack)
-// NODE_ENV: test or bundle development/production client/server app
+// BABEL_ENV: module definition system (cjs, es, or handled by webpack if undefined)
+// NODE_ENV: execution context (test or - bundle - development/production - client or server app)
 const { BABEL_ENV, NODE_ENV } = process.env
 const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'cjs' : false
-const targets = NODE_ENV === 'test' ? { node: true } : BABEL_ENV === 'test' ? { esmodules: true } : undefined
+const targets = NODE_ENV === 'test' ? { node: true } : BABEL_ENV === 'es' ? { esmodules: true } : undefined
+const { corejs, useBuiltIns } = NODE_ENV === 'test' ? { corejs: 3, useBuiltIns: 'usage' } : {}
 
 module.exports = {
-    presets: [['@babel/preset-env', { corejs: 3, modules, targets, useBuiltIns: 'usage' }]],
+    presets: [['@babel/preset-env', { corejs, modules, targets, useBuiltIns }]],
 }
