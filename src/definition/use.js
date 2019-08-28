creativewave/use-definition

commit 1be4c99170a42678dcfe5800aec919e15bb7e3cb

feat(animateTo-options) Add options as animateTo params

src/definition/use.js
@@ -15,6 +15,7 @@ const defaultOptions = {
     minDuration: 2000,
     precision: 2,
     startIndex: 0,
+    timing: 'easeOutCubic',
 }
 
 /**
@@ -43,10 +44,12 @@ const defaultOptions = {
  */
 const useDefinition = ({ definitions, options: userOptions = {} }) => {
 
-    const { startIndex, ...options } = { ...defaultOptions, ...userOptions }
-    const defs = React.useMemo(() => setAnimations(normalize(parse(definitions)), options), [definitions, options])
+    const { startIndex, ...globalOptions } = { ...defaultOptions, ...userOptions }
+    const defs = React.useMemo(
+        () => setAnimations(normalize(parse(definitions)), globalOptions),
+        [definitions, globalOptions])
     const [definition, setDefinition] = React.useState(defs[startIndex])
-    const animation = React.useRef({ index: startIndex })
+    const animation = React.useRef({ index: startIndex, isRunning: false })
 
     /**
      * animateTo :: NextIndex -> TimingFunction -> { sequence: Task, run: Frame -> TaskExecution }
@@ -61,14 +64,15 @@ const useDefinition = ({ definitions, options: userOptions = {} }) => {
      *
      * Memo: the scoped `definition` will be stale right after animation starts.
      */
-    const animateTo = (nextIndex, pointTimingFunction = 'easeOutCubic') => {
+    const animateTo = (next, stepOptions = {}) => {
 
-        const from = animation.current.isRunning ? definition : defs[animation.current.index]
-        const next = typeof nextIndex === 'string'
+        const nextIndex = typeof next === 'string'
             ? animation.current.index === defs.length - 1 ? 0 : animation.current.index + 1
-            : nextIndex
-        const to = defs[next]
-        const timingFunction = parseTimingFunction(pointTimingFunction)
+            : next
+        const options = { ...globalOptions, ...stepOptions }
+        const from = animation.current.isRunning ? definition : defs[animation.current.index]
+        const to = defs[nextIndex]
+        const timingFunction = parseTimingFunction(options.timing)
         const timeFunction = time => {
 
             let hasRun = true
@@ -76,7 +80,7 @@ const useDefinition = ({ definitions, options: userOptions = {} }) => {
             setDefinition(from.map(({ points, type }, commandIndex) => ({
                 points: points.map((group, groupIndex) => {
 
-                    const { delay, duration } = to[commandIndex].points[groupIndex]
+                    const { delay = options.delay, duration = options.duration } = to[commandIndex].points[groupIndex]
                     const hasStarted = time > delay
                     const hasFrame = hasStarted && time < duration + delay
 
@@ -111,11 +115,11 @@ const useDefinition = ({ definitions, options: userOptions = {} }) => {
             sequence: animate(timeFunction)
                 .and(Task.of().map(() => {
                     animation.current.isRunning = true
-                    animation.current.index = next
+                    animation.current.index = nextIndex
                 }))
                 .map(() => {
                     animation.current.isRunning = false
-                    return next
+                    return nextIndex
                 })
                 .orElse(logReject('[use-definition-hook]: error while running animation.')),
         }

commit a967dc7638b1b4a956c78b1f7607698eea30aa84

fix(src/definition/use) Memoize gathered options (regression from 1be4c99)

src/definition/use.js
@@ -18,6 +18,34 @@ const defaultOptions = {
     timing: 'easeOutCubic',
 }
 
+const shallowEqual = (prev, current) => {
+    for (const prop in prev)
+        if (prev[prop] !== current[prop])
+            return false
+    return true
+}
+
+/**
+ * useGatherMemo :: [Object, ...String|Symbol] -> [a, Object]
+ */
+const useGatherMemo = (object, ...props) => {
+
+    const ref = React.useRef(object)
+    const rest = React.useRef({})
+
+    if (!shallowEqual(ref.current, object)) ref.current = object
+
+    return React.useMemo(
+        () => Object.keys(ref.current).reduce(
+            (gather, key) => {
+                if (props.includes(key)) return gather
+                gather[gather.length - 1][key] = ref.current[key]
+                return gather
+            },
+            props.map(prop => ref.current[prop]).concat(rest.current)),
+        [ref, rest, props])
+}
+
 /**
  * getNextIndex :: { length: Number } -> Number -> Number
  */
@@ -70,7 +98,7 @@ const reducer = (state, action) => {
  */
 const useDefinition = (definitions, userOptions = {}) => {
 
-    const { startIndex, ...globalOptions } = { ...defaultOptions, ...userOptions }
+    const [startIndex, globalOptions] = useGatherMemo({ ...defaultOptions, ...userOptions }, 'startIndex')
     const defs = React.useMemo(
         () => setAnimations(normalize(parse(definitions)), globalOptions),
         [definitions, globalOptions])